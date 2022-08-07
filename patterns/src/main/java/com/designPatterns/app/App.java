347b4756de2a645b2ba64739569d8bf8662e8489
EC2 Default User <ec2-user@ip-172-31-35-251.ap-south-1.compute.internal>
2022-08-07 17:08:00 +0000

    added patterns maven project base

---
+++ patterns/src/main/java/com/designPatterns/app/App.java
@@ -0,0 +1,24 @@
+package com.designPatterns.app;
+
+import com.designPatterns.app.solid.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+    
+    public static void main( String[] args )
+    {
+        
+        S obj = new S();
+        System.out.println( "Hello World!" );
+        obj.p();    
+        
+        
+    }
+    
+    
+}
---
+++ patterns/src/main/java/com/designPatterns/app/solid/S.java
@@ -0,0 +1,13 @@
+package com.designPatterns.app.solid;
+
+
+
+public class S {
+
+	public static void p(){
+		System.out.println("Finally");
+	}
+
+
+}
+
